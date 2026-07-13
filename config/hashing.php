f9f9cebcf263e6a2c263502578f7e5a2cc4fc65b
juanetsolutions-code <juanetsolutions@gmail.com>
2026-07-13 22:14:11 +0300

    chore: initialize base framework configuration
    Add core Laravel configuration files for mail, hashing, views, and CORS, along with essential public directory assets and bootstrap cache ignoring.

---
+++ config/hashing.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Hash Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default hash driver that will be used to hash
+    | passwords for your application. By default, the bcrypt algorithm is
+    | used; however, you remain free to modify this option if you wish.
+    |
+    */
+
+    'driver' => 'bcrypt',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bcrypt Options
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the configuration options for the Bcrypt bcrypt
+    | driver. This will control the amount of work required to hash the
+    | passwords using the bcrypt algorithm.
+    |
+    */
+
+    'bcrypt' => [
+        'rounds' => env('BCRYPT_ROUNDS', 12),
+        'verify' => true,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Argon Options
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the configuration options for the Argon argon
+    | driver. This will control the CPU and memory resource cost required
+    | to hash the passwords using the Argon2 algorithm.
+    |
+    */
+
+    'argon' => [
+        'memory' => 65536,
+        'threads' => 1,
+        'time' => 4,
+        'verify' => true,
+    ],
+
+];
